ea7b6511b1c888d5f38ab273c2f088926b1f5002
Jaeyun-18 <min070118@gmail.com>
2026-07-03 15:43:16 +0900

    중간점검

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", index, name="accounts-index"),
     path("login/", login, name="accounts-login"),
     path("signup/", signup, name="accounts-signup"),
+    path("<str:username>/", index, name="user-profile"),
 ]
