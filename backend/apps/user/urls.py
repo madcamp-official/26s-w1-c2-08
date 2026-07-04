af7b437aac362cbf8458d6f0fe9f6e1b97fef5fa
Jaeyun-18 <min070118@gmail.com>
2026-07-04 17:19:30 +0900

    user_list api 및 jsx 생성

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import user_profile
+from .views import user_profile, user_list
 
 urlpatterns = [
+    path('', user_list, name='user-list'),
     path("<str:username>/", user_profile, name="user-profile"),
 ]
